b62912f9e777f23ee9b8e0be98413900fcf301aa
Joyce Oliveira <joycerebeca4@gmail.com>
2022-04-12 23:48:01 -0300

    variaveis

---
+++ modulo1/variaveis/index.js
@@ -0,0 +1,84 @@
+let a = 10
+let b = 10
+
+console.log(b)
+
+b = 5
+console.log(a, b)
+
+// a = 10
+// b = 5
+
+let a = 10
+let b = 20
+c = a
+b = c
+a = b
+
+console.log(a, b, c) 
+
+/* 
+ a = 10
+ b = 10
+ c = 10
+ */
+
+let p = prompt("Quantas horas você trabalha por dia?")
+let t = prompt("Quanto você recebe por dia?")
+alert(`Voce recebe ${t/p} por hora`)
+
+
+ // let horas = prompt("Quantas horas você trabalha por dia?");
+ // let horasEmNumero = Number(horas);
+ // let valor = prompt("Quanto você recebe por dia?");
+ // let valorEmNumero = Number(valor);
+ // alert(`Voce recebe ${valorEmNumero/horasEmNumero} por hora`);
+
+
+
+
+let nomeDaPessoa = 
+let idade = 
+
+console.log(typeof nomeDaPessoa);
+console.log(typeof idade);
+
+// não abriu no console, deu erro
+
+let nomeDaPessoa = null
+console.log(nomeDaPessoa)
+
+const nomeDaPessoa = prompt("Qual seu nome?");
+let idade = prompt("Qual a sua idade?");
+
+console.log(typeof nomeDaPessoa);
+console.log(typeof idade);
+
+console.log("Olá", nomeDaPessoa, ",", "você tem", idade "anos")
+
+prompt("Você namora?" resposta)
+
+let resposta = null
+prompt("Você namora?")
+prompt("Você estuda?")
+prompt("Você mora no Brasil?")
+
+console.log("Você namora?" resposta)
+console.log("Você estuda?" resposta)
+console.log("Você mora no Brasil?" resposta)
+
+
+// let a = 10
+// let b = 25
+// let c = a
+// a = b
+// b = c
+
+// console.log("o novo valor de a é" a)
+O novo valor de a é 25
+// console.log("o novo valor de b é" b)
+O novo valor de b é 10
+
+
+
+
