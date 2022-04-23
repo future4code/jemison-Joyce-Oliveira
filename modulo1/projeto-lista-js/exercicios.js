2d2223a527e246eec8f3735c779e233fadc5a1db
Joyce Oliveira <joycerebeca4@gmail.com>
2022-04-22 23:44:44 -0300

    projeto lista js

---
+++ modulo1/projeto-lista-js/exercicios.js
@@ -0,0 +1,180 @@
+// EXEMPLOS DE IMPLEMENTAÇÃO ---------------------------------------------------------------
+
+// EXERCÍCIO 0A 
+/*function soma(num1, num2) {
+ // implemente sua lógica aqui
+ const somando = num1 + num2
+ console.log(somando)
+ return num1 + num2
+}
+
+soma(1,2)
+soma(4,5)
+
+
+// EXERCÍCIO 0B
+function imprimeMensagem() {
+  // implemente sua lógica aqui
+  const mensagem = prompt('Digite "Olá, mundo!"')
+
+  console.log(mensagem)
+}
+
+imprimeMensagem()
+
+ //EXERCÍCIOS PARA FAZER ------------------------------------------------------------------
+
+// EXERCÍCIO 01
+ function calculaAreaRetangulo(altura, largura) {
+  // implemente sua lógica aqui
+  
+  const alturaRetangulo = prompt('Qual a altura de um retângulo?')
+  const larguraRetangulo = prompt('Qual a largura de um retângulo?')
+  const retanguloInteiro = alturaRetangulo * larguraRetangulo 
+  console.log(retanguloInteiro)
+}
+
+calculaAreaRetangulo()
+calculaAreaRetangulo()
+
+ //EXERCÍCIO 02 
+ function imprimeIdade() {
+  // implemente sua lógica aqui
+  const ano = prompt('Qual o ano atual?')
+  const anoNascimento = prompt('Qual o ano do seu nascimento?')
+  const calculandoIdade = ano - anoNascimento
+  console.log(calculandoIdade)
+}
+
+imprimeIdade()
+imprimeIdade() 
+
+  // EXERCÍCIO 03 
+ /*function calculaIMC(peso, altura) {
+  // implemente sua lógica aqui
+  const imc = (peso/(altura*altura))
+  console.log(imc)
+}
+
+calculaIMC(85, 1.8)
+calculaIMC(70, 1.65)
+
+
+
+ // EXERCÍCIO 04 
+ /*function imprimeInformacoesUsuario() {
+  // implemente sua lógica aqui
+  // "Meu nome é NOME, tenho IDADE anos, e o meu email é EMAIL."
+  
+  const nomesUser = prompt(`Qual o seu nome?`)
+  const idadesUser = prompt(`Qual a sua idade?`)
+  const emailUser = prompt(`Qual o seu email?`)
+  const resultFrase = `Meu nome é ${nomesUser}, tenho ${idadesUser} anos, e o meu email é ${emailUser}.`
+console.log(resultFrase)
+} 
+
+imprimeInformacoesUsuario() 
+imprimeInformacoesUsuario() 
+
+// EXERCÍCIO 05 
+/*function imprimeTresCoresFavoritas() {
+  // implemente sua lógica aqui
+const cor1 = prompt(`Qual sua cor favorita?`)
+const cor2 = prompt(`Qual sua cor favorita?`)
+const cor3 = prompt(`Qual sua cor favorita?`)
+const coresFavoritas = [cor1, cor2, cor3]
+console.log(coresFavoritas) 
+}
+
+imprimeTresCoresFavoritas()
+imprimeTresCoresFavoritas()
+
+
+
+// EXERCÍCIO 06
+/*function retornaStringEmMaiuscula(string) {
+  // implemente sua lógica aqui
+const palavraMaiscula = string.toUpperCase()
+console.log(palavraMaiscula)
+}
+
+retornaStringEmMaiuscula("oi")
+retornaStringEmMaiuscula("bAnAnA")
+
+// EXERCÍCIO 07
+function calculaIngressosEspetaculo(custo, valorIngresso) {
+  // implemente sua lógica aqui
+ //const custoTeatro = 3000
+ //const valorDoIngresso = 100
+ //const calculo = custoTeatro/valorDoIngresso
+ 
+
+ const custoTeatro2 = 5500
+ const valorDoIngresso2 = 50
+ 
+ const segundoCalculo = custoTeatro2/valorDoIngresso2
+ 
+
+ const resultaDosGastos = `${calculo} ${segundoCalculo}`
+ 
+ 
+
+console.log(resultaDosGastos)
+
+return resultaDosGastos
+
+
+
+}
+
+calculaIngressosEspetaculo()
+calculaIngressosEspetaculo()
+
+
+// EXERCÍCIO 08
+/*function checaStringsMesmoTamanho(string1, string2) {
+  // implemente sua lógica aqui
+
+}
+
+// EXERCÍCIO 09
+function retornaPrimeiroElemento(array) {
+  // implemente sua lógica aqui
+
+}
+
+// EXERCÍCIO 10
+function retornaUltimoElemento(array) {
+  // implemente sua lógica aqui
+
+}
+
+// EXERCÍCIO 11
+function trocaPrimeiroEUltimo(array) {
+  // implemente sua lógica aqui
+
+}
+
+// EXERCÍCIO 12
+function checaIgualdadeDesconsiderandoCase(string1, string2) {
+  // implemente sua lógica aqui
+
+}
+
+// EXERCÍCIO 13
+function checaRenovacaoRG() {
+  // implemente sua lógica aqui
+
+}
+
+// EXERCÍCIO 14
+function checaAnoBissexto(ano) {
+  // implemente sua lógica aqui
+
+}
+
+// EXERCÍCIO 15
+function checaValidadeInscricaoLabenu() {
+  // implemente sua lógica aqui
+
+}*/
