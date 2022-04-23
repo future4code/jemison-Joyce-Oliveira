6951ff70f727898b5578cf3a730f15fa4387872e
Joyce Oliveira <joycerebeca4@gmail.com>
2022-04-22 23:38:52 -0300

    funcoes

---
+++ modulo1/funcoes/index.js
@@ -0,0 +1,130 @@
+/*function minhaFuncao(variavel) {
+	return variavel * 5
+
+ }
+
+console.log(minhaFuncao(2))
+console.log(minhaFuncao(10)) 
+// 10
+// 50
+
+minhaFuncao(2)
+minhaFuncao(10)
+// Não iria imprimir no console, pq não teve o comando de imprimir a função
+
+*/ /*let textoDoUsuario = prompt("Insira um texto");
+
+const outraFuncao = function(texto) {
+	return texto.toLowerCase().includes("cenoura")
+}
+
+const resposta = outraFuncao(textoDoUsuario)
+console.log(resposta)
+
+//a) essa função imprime a frase em minusculo e encontra a palavra cenoura na String,
+//retornando true ou false
+
+//b) retorna true para todas as entradas
+
+//Exercícios de escrita de código
+
+function informacoes() {
+    let nome = "Joyce"
+    let idade = "23"
+    let residencia = "Ipatinga"
+    let profissao = "Promotora"
+
+    const dizendo = (`Eu sou ${nome}, tenho ${idade} anos, moro em ${residencia}, e sou ${profissao} .`)
+console.log(dizendo)
+
+}
+
+informacoes()*/
+
+
+/*function informacoes2() {
+    let nome2 = "Laís"
+    let idade2 = Number(23)
+    let residencia2 = "São Paulo"
+    let profissao2 = "Instrutora"
+
+    const dizendo2 = (`Eu sou ${nome2}, tenho ${idade2} anos, moro em ${residencia2}, e sou ${profissao2}`)
+  console.log(dizendo2)
+
+}
+
+informacoes2()
+
+
+/*function recebe(numero1, numero2) {
+const number1 = 5
+const number2 = 7
+const result = number1+number2
+console.log(result)
+
+}
+
+recebe()
+
+
+let informa = 20 >= 10
+console.log(informa)
+
+function mensagem(string) {
+const imprime = "Olá, mundo"
+console.log(imprime.length)
+const ver = imprime.toUpperCase()
+console.log(ver)
+
+
+}
+
+mensagem()
+
+function operacao1() {
+    
+    const pergunta = prompt('Insira um número:')
+    const pergunta1 = prompt('Insira um número:')
+    const valor = 30
+    const valor1 = 3
+    let soma = valor + valor1
+    console.log(soma)
+}
+
+operacao1()
+
+function operacao2() {
+    
+    const pergunt2 = prompt('Insira um número:')
+    const pergunta3 = prompt('Insira um número:')
+    const valor2 = 30
+    const valor3 = 3
+    let soma1 = valor2 - valor3
+    console.log(soma1)
+}
+
+operacao2()
+
+function operacao3() {
+    
+    const pergunt4 = prompt('Insira um número:')
+    const pergunta5 = prompt('Insira um número:')
+    const valor4 = 30
+    const valor5 = 3
+    let soma2 = valor4 * valor5
+    console.log(soma2)
+}
+
+operacao3()*/
+
+function operacao3() {
+    
+    const pergunt6 = prompt('Insira um número:')
+    const pergunta7 = prompt('Insira um número:')
+    const valor6 = 30
+    const valor7 = 3
+    let soma3 = valor6/ valor7
+    console.log(soma3)
+}
+
+operacao3()
