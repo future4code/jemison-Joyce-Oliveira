00db969e5a0919df7c71326e820bdb5b7d256b32
Joyce Oliveira <joycerebeca4@gmail.com>
2022-04-28 22:13:57 -0300

    condicionais

---
+++ modulo1/condicionais/index.js
@@ -0,0 +1,44 @@
+const respostaDoUsuario = prompt("Digite o número que você quer testar")
+const numero = Number(respostaDoUsuario)
+
+if (numero % 2 === 0) {
+  console.log("Passou no teste.")
+} else {
+  console.log("Não passou no teste.")
+}
+// 6 
+passou no teste
+a) O teste verifica se o numero é falso ou verdadeiro
+dentro da sua lógica
+b) ele imprime passou no teste para os números pares
+c) para os numeros impares
+
+let fruta = prompt("Escolha uma fruta")
+let preco
+switch (fruta) {
+  case "Laranja":
+    preco = 3.5
+    break;
+  case "Maçã":
+    preco = 2.25
+    break;
+  case "Uva":
+    preco = 0.30
+    break;
+  case "Pêra":
+    preco = 5.5
+    break; // BREAK PARA O ITEM c.
+  default:
+    preco = 5
+    break;
+}
+console.log("O preço da fruta ", fruta, " é ", "R$ ", preco)
+
+const numero = Number(prompt("Digite o primeiro número."))
+
+if(numero > 0) {
+  console.log("Esse número passou no teste")
+	let mensagem = "Essa mensagem é secreta!!!"
+}
+
+console.log(mensagem)
