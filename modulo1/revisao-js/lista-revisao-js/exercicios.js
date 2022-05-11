7804c33d096cb51dbfcf8041e4ad7ebe465b639b
Joyce Oliveira <joycerebeca4@gmail.com>
2022-05-10 23:11:47 -0300

    revisao-js

---
+++ modulo1/revisao-js/lista-revisao-js/exercicios.js
@@ -0,0 +1,106 @@
+// ATENÇÃO!!!
+//    -> NÃO COMENTE NENHUMA DAS FUNÇÕES DECLARADAS!!! 
+//    -> NÃO MODIFIQUE OS PARÂMETROS DAS FUNÇÕES!!! ()
+
+
+// EXERCÍCIO 01
+/*function retornaTamanhoArray(array) {
+   let retornandoTamanho = [3, 2, 1, 4, 7]
+    console.log(retornandoTamanho.length)
+    return 
+}
+
+retornaTamanhoArray()
+
+// EXERCÍCIO 02
+function retornaArrayInvertido(array) {
+  const retornandoInverso = [0,8,23,16,10,15,41,12,13].reverse()
+  console.log(retornandoInverso)
+  return 
+}
+
+retornaArrayInvertido()
+
+// EXERCÍCIO 03
+function retornaArrayOrdenado(array) {
+  const retornandoOrdenado = [3, 2, 1, 4, 7].sort()
+  console.log(retornandoOrdenado)
+  return 
+  
+}
+
+retornaArrayOrdenado()
+
+// EXERCÍCIO 04
+function retornaNumerosPares(array) {
+const retornandoOrdenado = [3, 2, 1, 4, 7].filter()
+  const filtrandoPares = retornandoOrdenado.filter(-1)
+  return 
+  
+}
+retornaNumerosPares()
+
+// EXERCÍCIO 05
+/*function retornaNumerosParesElevadosADois(array) {
+ 
+}
+
+// EXERCÍCIO 06
+function retornaMaiorNumero(array) {
+  
+}
+
+// EXERCÍCIO 07
+function retornaObjetoEntreDoisNumeros(num1, num2) {
+
+}
+
+// EXERCÍCIO 08
+function retornaNPrimeirosPares(n) {
+   
+}
+
+// EXERCÍCIO 09
+function classificaTriangulo(ladoA, ladoB, ladoC) {
+
+}
+
+// EXERCÍCIO 10
+function retornaSegundoMaiorESegundoMenor(array) {
+  
+}
+
+// EXERCÍCIO 11
+function retornaChamadaDeFilme(filme) {
+   
+}
+
+// EXERCÍCIO 12
+function retornaPessoaAnonimizada(pessoa) {
+   
+}
+
+// EXERCÍCIO 13A
+function retornaPessoasAutorizadas(pessoas) {
+   
+}
+
+// EXERCÍCIO 13B
+function retornaPessoasNaoAutorizadas(pessoas) {
+  
+}
+
+// EXERCÍCIO 14
+function retornaContasComSaldoAtualizado(contas) {
+
+}
+
+// EXERCÍCIO 15A
+function retornaArrayOrdenadoAlfabeticamente(consultas) {
+  
+}
+
+// EXERCÍCIO 15B
+function retornaArrayOrdenadoPorData(consultas) {
+   
+}*/
