fae4caa8d643842736c10a5a7934ca8ab7f33ead
Joyce Oliveira <joycerebeca4@gmail.com>
2022-05-12 21:12:54 -0300

    resolvendo-problemas

---
+++ modulo1/resolvendo-problemas/index.js
@@ -0,0 +1,13 @@
+/* Existem 7 passos muito importantes para resolver e solucionar problemas no código que ajudam no processo que queremos
+alcançar. O 1º passo é ter atenção no enunciado, se atentar nas informações e fazer questionamentos, destacando 
+informações relevantes e importantes para a resolução do mesmo. O 2º passo é retirar informações relevantes, é aqui que 
+iniciaremos a fase de pensar e criar palavras chaves, nomes das variáveis que são legíveis e estejam compatíveis com a 
+temática do enunciado. O 3º passo é pensar entradas e saídas questionando quais dados serão recebidos, formular respostas
+que condizem com as entradas e saídas pensadas. O 4º passo é fazer um esquema visual é criar exemplos com a lógicos de 
+forma lúdica que trazem maior clareza sobre o cenário que está sendo contextualizado. O 5º passo é criar código feio 
+pensados para resolver e solucionar o problema, criar e filtrar as melhores soluções e testá-las. O 6º passo é testar 
+força bruta, consiste em trazer uma solução obvia com clareza e entendimento, sendo légivel de uma forma que a pessoa 
+que esta codando possa raciocinar e posteriormente trascrever para a linguagem de programação. O 7º passo é refatorar, 
+analisar e vizualiizar no final se todo o contéudo está de forma clara, legível e limpa e que as pessoas que forem 
+vizualizá-lo possam entender e não se perder na lógica, por isso é importante ser de fácil entendimento e funcional, 
+tendo cuidado para não quebrar no momento de refatorar. */ 
